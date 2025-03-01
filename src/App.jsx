859474c9a13716b273f1e6facf0a502b541e0d1e
sendhu2821 <senthamizh764@gmail.com>
2025-03-01 12:14:38 +0530

    add AddJob page and update state functionality

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import MainLayout from "./layouts/MainLayout";
 import JobsPage from "./pages/JobsPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import JobPage, { Loader } from "./pages/JobPage";
+import AddJob from "./pages/AddJob";
 
 const App = () => {
   const router = createBrowserRouter(
@@ -17,6 +18,7 @@ const App = () => {
       <Route path="/" element={<MainLayout />}>
         <Route index element={<HomePage />} />
         <Route path="/jobs" element={<JobsPage />} />
+        <Route path="/add-job" element={<AddJob />} />
         <Route path="/jobs/:id" element={<JobPage />} loader={Loader} />
         <Route path="*" element={<NotFoundPage />} />
       </Route>
